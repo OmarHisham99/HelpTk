2408e23538fe31de4ce9fdae85a844f91491188a
OmarHisham99 <ohisham413@gmail.com>
2023-06-27 19:14:04 +0300

    about us

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from 'src/app/pages/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import { BodyComponent } from './pages/body/body.component';
+import { AboutusComponent } from './pages/aboutus/aboutus.component';
+import { Aboutus2Component } from './pages/aboutus2/aboutus2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    AboutusComponent,
+    Aboutus2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/aboutus2/aboutus2.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Aboutus2Component } from './aboutus2.component';
+
+describe('Aboutus2Component', () => {
+  let component: Aboutus2Component;
+  let fixture: ComponentFixture<Aboutus2Component>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [Aboutus2Component]
+    });
+    fixture = TestBed.createComponent(Aboutus2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
