7f039bbf7d0bf693f8e3a35101b7d7d0d8951494
omar-abnour <Omar-abnuur>
2023-07-23 17:38:25 +0300

    some responsive bugs

--- src/app/pages/our-news/our-news.component.ts
+++ src/app/pages/our-news/our-news.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { WebDataService } from 'src/app/web-data.service';
 
 @Component({
   selector: 'app-our-news',
   templateUrl: './our-news.component.html',
-  styleUrls: ['./our-news.component.css']
+  styleUrls: ['./our-news.component.css'],
 })
 export class OurNewsComponent {
+  ourNews: any;
 
+  constructor(
+    private webDataService: WebDataService,
+    public translate: TranslateService
+  ) {
+    this.fetchData();
+  }
+
+  DateFormatter(date: string): string {
+    return date.split('T')[0] + ' - ' + date.split('T')[1].split('.')[0];
+  }
+
+  fetchData(): void {
+    this.webDataService.getOurNews().subscribe((data) => {
+      this.ourNews = data.data;
+      console.log(this.ourNews);
+    });
+  }
 }
