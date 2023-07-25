55561b268a16ad17a6a2053af872e599b6bb0aef
michael-abnour <michael_micheal@myabnuur.me>
2023-07-26 00:49:32 +0300

    working in responsive

--- src/app/pages/body/body.component.ts
+++ src/app/pages/body/body.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef } from '@angular/core';
 import { WebDataService } from 'src/app/web-data.service';
 import { TranslateService } from '@ngx-translate/core';
+import { ScrollService } from 'src/app/services/scrollService';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
@@ -12,10 +13,20 @@ export class BodyComponent {
   constructor(
     private webDataService: WebDataService,
     private elementRef: ElementRef,
-    public translate: TranslateService
+    public translate: TranslateService,
+    private scrollService: ScrollService
   ) {
     this.fetchData();
     this.youtubeLink = 'https://www.youtube.com/embed/';
+  }
+
+  scrollToId(id: string) {
+    console.log('element id : ', id);
+    this.scrollService.scrollToElementById(id);
+  }
+
+  scrollToElement(element: HTMLElement) {
+    this.scrollService.scrollToElement(element);
   }
 
   fetchData(): void {
@@ -29,4 +40,8 @@ export class BodyComponent {
       element.setAttribute('src', this.youtubeLink);
     });
   }
+
+  scrollToNextSection = () => {
+    alert('Scrooll');
+  };
 }
